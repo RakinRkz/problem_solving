ab1921092388df134c8956cc403d115ff6d46c47
rakinrkz <rakinuzzaman@gmail.com>
2023-02-04 12:00:15 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -16,6 +16,7 @@ int main(){
         for(int i=0; i<n; i++){
             cout<<a[i];
         }
+        cout<<endl;
         int cnt = 0;
         for(int k=0; k<n-1; k++){
             if(k) break;
