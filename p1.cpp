8b5d405e7aef3603fc947f007e0ed6a5c2b077e3
rakinrkz <rakinuzzaman@gmail.com>
2023-02-05 22:52:25 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -1,61 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-map<int, int> factors;
-void primeFactors(int n)
-{
-    // Print the number of 2s that divide n
-    while (n % 2 == 0)
-    {
-        factors[2]++;
-        n = n/2;
-    }
- 
-    // n must be odd at this point. So we can skip
-    // one element (Note i = i +2)
-    for (int i = 3; i <= sqrt(n); i = i + 2)
-    {
-        // While i divides n, print i and divide n
-        while (n % i == 0)
-        {
-            factors[i]++;
-            n = n/i;
+int func(string s){
+    int sum = 0;
+    int chars[26] = {0};
+    for(int i=0; i<s.length(); i++){
+        int ch = s[i] - 'a';
+        if(chars[ch] == 0){
+            sum ++;
+            chars[ch] ++;
         }
     }
- 
-    // This condition is to handle the case when n
-    // is a prime number greater than 2
-    if (n > 2)
-        factors[n]++;
+    return sum;
 }
 int main(){
     int t;
-    
     cin>>t;
     for(int tt=0; tt<t; tt++){
         int n;
+        string str;
         cin>>n;
+        cin>>str;
 
-        factors.clear();
-        primeFactors(n);
-        
-        map<int, int>::iterator mit;
-        long long sum = 0;
-        long long tmp;
-        while(true){
-            tmp = 1;
-            for(mit = factors.begin(); mit != factors.end(); mit++){
-                if(mit->second >= 1)
-                {
-                    mit->second --;
-                    tmp *= mit->first;
-                }
-            }
-            if(tmp != 1)
-                sum += tmp;
-            else
-                break;
+        int mx = -1;
+        for(int i=0; i<n; i++){
+            cout<<str.substr(0, i)<<endl;
+            // mx = max(mx, func(str.substr(0, i)));
+            cout<<str.substr(i, n-i-1)<<endl;
+            // mx = max(mx, func(str.substr(i, n-i-1)));
         }
-        cout<<sum<<endl;
-        
     }
 }
