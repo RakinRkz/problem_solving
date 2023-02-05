8705b538474d636a4fd9c4a01ddcfa39558bc57d
rakinrkz <rakinuzzaman@gmail.com>
2023-02-05 22:54:22 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -23,10 +23,11 @@ int main(){
 
         int mx = -1;
         for(int i=0; i<n; i++){
-            cout<<str.substr(0, i)<<"   ";
-            // mx = max(mx, func(str.substr(0, i)));
-            cout<<str.substr(i, n-i)<<endl;
-            // mx = max(mx, func(str.substr(i, n-i-1)));
+            // cout<<str.substr(0, i)<<"   ";
+            mx = max(mx, func(str.substr(0, i)));
+            // cout<<str.substr(i, n-i)<<endl;
+            mx = max(mx, func(str.substr(i, n-i)));
         }
+        cout<<mx<<endl;
     }
 }
