f3e50cfb94cb188654329ae47b5fc6427b62ece3
rakinrkz <rakinuzzaman@gmail.com>
2023-02-05 23:20:50 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int func(string s){
     int sum = 0;
     int chars[26] = {0};
@@ -10,10 +11,11 @@ int func(string s){
             chars[ch] ++;
         }
     }
-
     return sum;
 }
+
 int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;
     cin>>t;
     for(int tt=0; tt<t; tt++){
