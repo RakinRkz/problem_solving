4abdf2b666afedaa873a8dcfa21136659d6b5ba0
rakinrkz <rakinuzzaman@gmail.com>
2023-02-07 19:02:49 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -1,36 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int func(string s){
-    int sum = 0;
-    int chars[26] = {0};
-    for(auto ch: s) chars[ch - 'a']++;
-
-    int op[26] = {0};
-
-    for(int i=0; i<s.length(); i++){
-        int ch = s[i] - 'a';
-        chars[ch]--, op[ch]++;
-
-        int tmp = 0;
-        for(int j=0; j<26; j++){
-            tmp += min(1, chars[j]) + min(1, op[j]);
-        }
-        sum = max(sum, tmp);
-    }
-    return sum;
-}
-
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;
     cin>>t;
     for(int tt=0; tt<t; tt++){
         int n;
-        string str;
-        cin>>n;
-        cin>>str;
+        cin >> n;
+        vector<vector<int>> arr(n, vector<int> (n-1));
+        // int arr[n][n];
 
-        cout<<func(str)<<endl;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1; j++){
+                cin >> arr[i][j];
+            }
+        }
+        int suffix;
+        int head;
+
+        map<int, vector<int>> mp;
+
+        for(int i=0; i<n; i++){
+            mp[arr[i][0]].push_back(i);
+        }
+        cout<<suffix<<endl<<head<<endl;
+        
+
     }
 }
