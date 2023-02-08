7cf68a9a172ecbbc59997e2aa5559250a0d0abc3
rakinrkz <rakinuzzaman@gmail.com>
2023-02-08 21:49:34 +0600

    p1

--- p1.cpp
+++ p1.cpp
@@ -6,12 +6,34 @@ int main(){
     int t;
     cin>>t;
     for(int tt=0; tt<t; tt++){
-        char c;
-        cin>>c;
-        string chk = "cdefors";
-        if (chk.find(c) != std::string::npos)
-            cout<<"yes\n";
-        else
-            cout<<"no\n";
+        int n;
+        cin >> n;
+        vector<int> v;
+        for (int i=1; i<=n*n; i++){
+            if(i&1)
+                v.push_back(i);
+            else
+                v.push_back(n*n - i);
+        }
+
+        int output[n][n];
+
+        for(int i=0, vx=0; i<n; i++){
+            if(i&1)
+                for(int j=0; j<n; j++, vx++){
+                    output[i][j] = v[vx];
+                }
+            else
+                for(int j=n-1; j>=0; j--, vx++){
+                    output[i][j] = v[vx];
+                }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0;j<n; j++)
+                cout<<output[i][j]<<' ';
+            cout<<endl;
+        }
+
     }
 }
